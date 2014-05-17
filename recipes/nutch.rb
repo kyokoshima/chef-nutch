f797d42f8b983ccb0241760fde4cac259088ae11
kyokoshima <dev.kyoko@gmail.com>
2014-05-17 09:20:26 +0900

    crawlコマンドまで作成

--- recipes/nutch.rb
+++ recipes/nutch.rb
@@ -1,11 +1,30 @@
+execute "start_crawl" do
+	command <<-EOH
+		cd #{node['work_dir']}/#{node['nutch']['base_name']}-#{node['nutch']['version']}/runtime/local
+		bin/crawl urls crawl/crawldb http://localhost:8983/solr 1
+	EOH
+	action :nothing
+end
+
+execute "seed_create" do
+	command <<-EOH
+		cd #{node['work_dir']}/#{node['nutch']['base_name']}-#{node['nutch']['version']}/runtime/local
+		mkdir urls
+		echo 'http://news.yahoo.co.jp/' >> urls/seed.txt
+	EOH
+	action :nothing
+	not_if { File.exist?("#{node['nutch']['work_dir']}/#{node['nutch']['base_name']}-#{node['nutch']['version']}/runtime/local/urls") }
+	notifies, :run, resouces(:execute => "start_crawl")
+end
+
 execute "ant-build" do
 	command <<-EOH
 		cd #{node['work_dir']}/#{node['nutch']['base_name']}-#{node['nutch']['version']}
 		ant clean runtime
 	EOH
 	action :nothing
+	notifies :run, resources(:execute => "seed_create")
 end
-
 template "nutch-site.xml" do
 	source "nutch-site.xml.erb"
 	path "#{node['work_dir']}/#{node['nutch']['base_name']}-#{node['nutch']['version']}/conf/nutch-site.xml"
